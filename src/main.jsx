46451734d5f36ea2d434ee8cc957b17102da172e
Alexander Rock <nynexman4464@gmail.com>
2026-09-16 13:57:59 -0500

    Switch to Y2K theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import '@astryxdesign/core/reset.css';
 import '@astryxdesign/core/astryx.css';
-import '@astryxdesign/theme-gothic/theme.css';
+import '@astryxdesign/theme-y2k/theme.css';
 import './extras.css';
 
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Theme } from '@astryxdesign/core/theme';
-import { gothicTheme } from '@astryxdesign/theme-gothic/built';
+import { y2kTheme } from '@astryxdesign/theme-y2k/built';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Theme theme={gothicTheme} mode="dark">
+    <Theme theme={y2kTheme} mode="dark">
       <App />
     </Theme>
   </React.StrictMode>,
